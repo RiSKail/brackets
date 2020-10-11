0eecb02c5cae6fdb0dd1e25ab461e409d998f6c0
Egor <44815359+RiSKail@users.noreply.github.com>
2020-10-11 23:06:47 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let open = [], close = [];
+  if (str.length % 2 !== 0) return false;
+
+  open = bracketsConfig.flat().map((item, i) => {
+    if (i % 2 == 0) return item;
+  }).join("");
+
+  close = bracketsConfig.flat().map((item, i) => {
+    if (i % 2 != 0) return item;
+  }).join("");
+
+  let str2 = "";
+  for (let i = 0; i < open.length; i++) {
+    (!isNaN(open[i])) ? str2 += "(" : str2 += "(\\";
+    str2 += open[i];
+    (!isNaN(open[i])) ? str2 += ")(" : str2 += ")(\\";
+    str2 += close[i];
+    if (i != open.length - 1)
+      str2 += ")|";
+    else
+      str2 += ")";
+  }
+
+  let regexp = new RegExp(str2);
+
+  while (str.search(regexp) != -1) {
+    str = str.replace(regexp, '');
+  }
+  return (str.length == 0) ? true : false;
+};
